2af8478c4b2f0ed464f9e3c6c81a19aefc6c068e
Gus Jimenez <jimenezgustavoandres@gmail.com>
2023-04-06 13:50:14 -0400

    terra resources, variables and commands

--- main.tf
+++ main.tf
@@ -10,9 +10,14 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "SECRET_ACCESS_KEY"
-  secret_key = "SECRET_KEY"
 }
+
+variable "subnet_prefix" {
+  # default     = "10.0.66.0/24" # value if you dont pass anything
+  description = "cidr block for the subnet"
+  type        =  object({ cidr_block = string, name = string})
+}
+
 
 # Create VPC
 resource "aws_vpc" "prod-vpc" {
@@ -50,11 +55,11 @@ resource "aws_route_table" "prod-route-table" {
 # Create a Subnet
 resource "aws_subnet" "subnet-1" {
   vpc_id            = aws_vpc.prod-vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = var.subnet_prefix.cidr_block
   availability_zone = "us-east-1a" # Try to always hard code the availability zone
 
   tags = {
-    Name = "prod-subnet"
+    Name = var.subnet_prefix.name
   }
 }
 
@@ -103,7 +108,7 @@ resource "aws_security_group" "allow-web" {
   }
 }
 
-# Create a network interface with an IP in the subnet taht was previously created
+# Create a network interface with an IP in the subnet that was previously created
 resource "aws_network_interface" "web-server-nic" {
   subnet_id       = aws_subnet.subnet-1.id
   private_ips     = ["10.0.1.50"]
@@ -118,6 +123,14 @@ resource "aws_eip" "one" {
   depends_on                = [aws_internet_gateway.gw] # No need to point to ID, you want the whole object
 }
 
+# This command will output public_ip information on the console when we do 'terraform apply' command 
+# *** careful with this command as it will deploy your infrastructure to aws (this is dangerous if your on a prod environment).
+# If you just want the output use 'terraform output' command or 'terraform refresh' command. 
+# This avoids writing 'terraform state list' and then 'terraform state show {resource name}' to show specific information on that resource
+output "aws_public_ip" {
+  value       = aws_eip.one.public_ip
+}
+
 # Create Ubuntu server and install/enable apache2
  resource "aws_instance" "web-server-instance" {
   ami               = "ami-007855ac798b5175e"
@@ -129,6 +142,7 @@ resource "aws_eip" "one" {
     device_index = 0
     network_interface_id = aws_network_interface.web-server-nic.id
   }
+
   user_data = <<-EOF
               #!/bin/bash
               sudo apt update -y
@@ -140,3 +154,35 @@ resource "aws_eip" "one" {
     Name = "web-server"
   }
  }
+
+output "server_private_ip" {
+  value       = aws_instance.web-server-instance.private_ip
+}
+
+output "server_id" {
+  value       = aws_instance.web-server-instance.id
+}
+
+
+
+
+#### IMPORTANT COMMANDS ###
+
+# terraform init - Start working directory
+# terraform validate - Check that your configuration is valid
+# teraform plan - Show changes 
+# terraform apply - Create or update your infrastructure
+# terraform destroy - Destroy your infrastructure
+# -- auto-approve flag at the end of the command will pass the 'yes' question
+
+################################################################################################################################
+
+# terraform - shows list of Main commands and All other commands
+# terraform show - Show the current state or a saved plan in detail
+# terraform state list - show list of resources in your state
+# terraform state show {resource name} - Details on one specific resource example: 'terraform state show aws_eip.one' 
+# terraform output - Prints out all the outputs
+# terraform refresh - Refresh your states and releases outputs without deploying resources
+# terraform destroy -target {name_of_resource} - Delete a specific resource, example: 'terraform destory -target aws_instance.web-server-instance' would delete just the web server instance
+# terraform apply -target {name_of_resource} - Creates or updates just one resource listed in command
+# terraform apply -var-file {example.tfvars} - To apply one specific variable file to resource
---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+subnet_prefix = {cidr_block = "10.0.0.0/16", name = "prod-subnet"}
